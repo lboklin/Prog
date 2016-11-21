9ad49eb55ead6e8c08f8f4d816c6be0a9d049a5d
Ludvig Böklin <ludvig.boklin@protonmail.com>
2016-11-21 22:12:21 +0100

    Add residual death on explosion

--- game/scripts/Projectile.gd
+++ game/scripts/Projectile.gd
@@ -1,11 +1,12 @@
 extends KinematicBody2D
 
 
-const ADVANCE_SPEED = 2200.0
+const ADVANCE_SPEED = 2600.0
 const MAX_RANGE = 500
 const SAFE_RADIUS = 128
 
-onready var shape = self.get_node("CollisionShape")
+onready var shape_projectile = self.get_node("CollisionShapeProjectile")
+onready var shape_explosion = self.get_node("CollisionShapeProjectile")
 
 var destination = Vector2()
 var direction = Vector2(1,0)
@@ -21,23 +22,25 @@ func _animation_finished():
 func explode():
 
 	# Stop exploded projectiles from colliding with each other
-	if not self.shape.is_queued_for_deletion():
-		self.shape.set_trigger(true)
+#	if not self.shape_projectile.is_queued_for_deletion():
+#		self.shape_projectile.set_trigger(true)
 
 	# Set the explosion orientation based on how it collided
-	var col_norm = self.get_collision_normal()
-	var vel_at_impact = self.ADVANCE_SPEED * self.direction
-	var collision_vector = self.get_collider_velocity() * col_norm
-	var lost_vel = (vel_at_impact - collision_vector).length()
-	var spread_angle_deg_width = lerp(85, 45, lost_vel / self.ADVANCE_SPEED)
-	var spread_dir = vel_at_impact.slide(collision_vector).angle() - deg2rad(180)
+#	var col_norm = self.get_collision_normal()
+#	var vel_at_impact = self.ADVANCE_SPEED * self.direction
+#	var collision_vector = self.get_collider_velocity() * col_norm
+#	var lost_vel = (vel_at_impact - collision_vector).length()
+#	var spread_angle_deg_width = lerp(85, 45, lost_vel / self.ADVANCE_SPEED)
+#	var spread_dir = vel_at_impact.slide(collision_vector).angle() - deg2rad(180)
 
-	var explosion = self.get_node("Object/ExplosionForwards")
-	explosion.set_param(0, spread_dir)
-	explosion.set_param(1, spread_angle_deg_width)
+#	var explosion = self.get_node("Object/ExplosionForwards")
+#	explosion.set_param(0, spread_dir)
+#	explosion.set_param(1, spread_angle_deg_width)
 
 #	rot_dir = dir.angle() - deg2rad(180)
 #	self.get_node("Object/ExplosionForwards").set_rot(rot_dir)
+
+	self.shape_explosion.set_trigger(false)
 	get_node("Animation").play("Explode")
 
 
@@ -63,8 +66,10 @@ func _fixed_process(delta):
 					collider.die()
 		else:
 			self.move(motion)
-#			motion.y *= 2
-#			self.dist_traveled += motion.length()
+			motion.y *= 2
+			self.dist_traveled += motion.length()
+			if self.dist_traveled > SAFE_RADIUS:
+				self.shape_projectile.set_trigger(false)
 
 	if hit:
 		explode()
@@ -75,7 +80,7 @@ func _ready():
 
 	get_node("Animation").connect("finished", self, "_animation_finished")
 
-	self.shape.set_trigger(true)
+	self.shape_projectile.set_trigger(true)
 	var current_pos = self.get_global_pos()
 
 	var dist = self.destination - current_pos
@@ -85,10 +90,16 @@ func _ready():
 	dir.y *= 2
 	self.direction = dir.normalized()
 
-	# I don't know why I can't use the same rotation for all nodes
-	# in Object, but apparently it doesn't work that way
-	var rot_dir = self.direction.angle() - deg2rad(180)
+	dir = dir.normalized()
+
+#	var rot_dir = dir.angle()
+#	self.get_node("Object/ExplosionForwards").set_rot(rot_dir)
+#	var rot_dir = dir.angle() - deg2rad(180)
+#	self.get_node("Object/ExplosionBackwards").set_rot(rot_dir)
+
+	dir.y *= 0.5
+	var rot_dir = dir.angle() - deg2rad(180)
+#	rot_dir = rot_dir - deg2rad(180)
 	self.get_node("Object/EnergyBall").set_rot(rot_dir)
-	self.get_node("Object/ExplosionForwards").set_rot(rot_dir)
 
 	set_fixed_process(true)
