fcd68b7a753b0e45d528a83c246662b487bdf9be
Ludvig Böklin <ludvig.boklin@protonmail.com>
2016-11-25 23:07:13 +0100

    Make bots fight each other

--- game/scripts/Bot.gd
+++ game/scripts/Bot.gd
@@ -1,41 +1,44 @@
 extends "res://scripts/Character.gd"
 
 
-export var accuracy_percentage = 70
+export var accuracy_percentage = 80 # Better than a stormtrooper
+
+# Holds the active target to attack and pursue
+var target = null
 
 
 func _fixed_process(delta):
 
 	# Probabilities are a percentage of likelyhood within the timespan of a second
 
-	# Attacking
-	if success(35):
-
-		if get_parent().has_node("Player"):
-			var player = get_parent().get_node("Player")
-			var player_sprite = player.get_node("Sprite")
-			var player_size = player_sprite.get_texture().get_width() * player_sprite.get_scale().x
-			var radius = player_size * 100 / accuracy_percentage
-			var target_loc = Vector2()
-
-			if player.moving: # Attack target's dest
-				target_loc = player.jump_destination[0]
-			else: # Attack target's current pos
-				target_loc = player.get_pos()
-
-			self.attack_location = rand_loc(target_loc, 0, radius)
-
-#	# Probability of jumping
-#	if not moving:
-#		if success(80):
-#			var jump_dest = randloc(get_viewport().get_visible_rect())
-#			jump_destination.append(jump_dest)
-#			self.indicate(jump_dest, "move_to")
-#	else:
-#		if success(70):
-#			var jump_dest = randloc(get_viewport().get_visible_rect())
-#			jump_destination.append(randloc(get_viewport().get_visible_rect()))
-#			self.indicate(jump_dest, "move_to")
+	if success(45): # Maybe consider possibly attacking, perhaps
+		var awareness_area = self.get_node("AwarenessArea")
+		# Try to acquire a target
+		if self.target != null:
+			if awareness_area.overlaps_area(self.target): # If target in sight
+				# How much bot could miss - diameter of a prog is ~90
+				var radius = 90 * 100 / accuracy_percentage
+				var target_loc = Vector2()
+				if self.target.moving:
+					target_loc = self.target.jump_destination[0] # Attack target's dest
+				else:
+					target_loc = self.target.get_pos() # Attack target's current pos
+				self.attack_location = rand_loc(target_loc, 0, radius) # Generate where bot accidentally/actually aimed
+			elif not awareness_area.overlaps_body(self.target): # If target is lost
+				self.target = null # Give up
+		else:
+			var possible_targets = awareness_area.get_overlapping_areas()
+			if possible_targets.size() > 0:
+				var valid_targets = []
+				for target in possible_targets:
+					if target.is_in_group("Prog") and target != self:
+						valid_targets.append(target)
+				var target_count = valid_targets.size()
+				if target_count > 0:
+					var chosen_target = randi() % target_count # choose one of them at random
+					self.target = valid_targets[chosen_target] # make it bot's life goal to kill it.. for the moment
+				else:
+					self.target = null
 
 	# Maybe jump - More likely to queue jumps while already doing it
 	var want_to_jump = false
