f2e47101551e6ef460b3532407dfb2fd95acdc75
Ludvig Böklin <ludvig.boklin@protonmail.com>
2017-01-21 00:38:12 +0100

    Move to dict-based state system

--- game/scripts/Character.gd
+++ game/scripts/Character.gd
@@ -5,16 +5,15 @@ export(Color) var primary_color
 export(Color) var secondary_color
 
 const WEP_CD = 1.0  # Weapon cooldown
-const JUMP_CD = 0.2  # Jump cooldown after landing
+const JUMP_CD = 0.1  # Jump cooldown after landing
 const MAX_SPEED = 1500  # Max horizontal (ground) speed
 const MAX_JUMP_RANGE = 1000  # How far you can jump from any given starting pos
 const JUMP_Q_LIM = 2  # Jump queue limit
 
 # State enums
-enum {IDLE, DEAD, RESPAWNING, MOVING, ATTACKING, STUNNED, BUSY}
+enum Condition {OK, DEAD, RESPAWNING, STUNNED, BUSY}
+enum Action {IDLE, MOVING, ATTACKING}
 enum Power {ON, OFF}
-
-var state = IDLE setget set_state, get_state
 
 slave var slave_pos = Vector2()
 slave var slave_atk_loc = Vector2()
@@ -23,115 +22,106 @@ slave var slave_focus = Vector2()
 
 # Counters
 var points = 0
+#var time_of_death = null  # If applicable
 
-# Timers
-var stunned_timer = 0.0
-var action_timer = 0.0
 
-## Dicts  ##
+## Dicts
 
-var jumps = {
-	"active_jump_origin": null,
-	"destinations"		: []
-} setget set_jumps, get_jumps
+var p_state = {
+	"condition"			: Condition.OK,
+	"action"			: Action.IDLE,
+	"action_timer"		: 0.0,
+	"position"			: Vector2(),
+	"motion"			: Vector2(),  # Horizontal
+	"height"			: 0  # Vertical
+} setget set_state, get_state
 
-var weapon_state = {
-	"state" 			: Power.ON,
+# Add timers when applied and remove when they expire
+var p_condition_timers = {
+#	"stunned"			: 0.0,
+#	"dead"				: 0.0,
+#	"respawning"		: 0.0,
+#	"busy"				: 0.0,
+} setget apply_condition_timer, get_condition_timers
+
+var p_path = {
+	"from"				: null,
+	"to"				: [],  # Take note that this is a jump queue array
+} setget set_path, get_path
+
+var p_weapon_state = {
+	"power" 			: Power.ON,
 	"target_loc"		: null,
-	"cooldown_timer"	: 0.0
+	"timer"	: 0.0
 } setget set_weapon_state, get_weapon_state
 
-var shield_state = {
-	"state"				: Power.ON,
-	"duration_timer"	: 2.0  # Spawn in with 2 sec protective shield
+var p_shield_state = {
+	"power"				: Power.ON,
+	"timer"				: 2.0  # Spawn in with 2 sec protective shield
 } setget set_shield_state, get_shield_state
 
-var respawn_state = {
-	"time_of_death" 	: null,
-	"respawn_timer" 	: 0.0
-} setget set_respawn_state, get_respawn_state
 
-
-##########################
+###########################
 ## SetGetters / queries  ##
-##########################
+###########################
 
 #-------------
 # General state
 
-func set_state(new_st):
-	if not is_state(new_st):
-		if is_state(MOVING):
-			stop_moving()
-	state = new_st
-	return new_st
+sync func set_state(new_state):
+	# Explain myself here please.
+	# 	re: Okay so it's probably that if the state disallows actions
+	# 		we should just set current action to Action.IDLE.
+	if ( new_state["condition"] != Condition.OK ) and ( new_state["condition"] != Condition.BUSY ):
+		new_state["action"] = Action.IDLE
+
+	p_state = new_state
+	return
 
 func get_state():
-	return state
+	return p_state
 
-func is_state(st):
-	return true if st == get_state() else false
+#-------------
+# General state
+
+func apply_condition_timer(condition, value):
+	p_condition_timers[condition] = value
+
+func get_condition_timers():
+	return p_condition_timers
 
 #-------------
 #-------------
 # Jump state
 
-func set_jumps(origin, dests):
-	jumps = {
-	"active_jump_origin" : origin,
-	"destinations" : dests
-	}
-
+sync func set_path(new_path):
+	p_path = new_path
 	return
 
-func get_jumps():
-	return jumps
+func get_path():
+	return p_path
 
 #-------------
 #-------------
 # Weapon state
 
-func set_weapon_state(power, t_loc, cd):
-	weapon_state = {
-	"state" 			: power,
-	"target_loc"		: t_loc,
-	"cooldown_timer"	: cd
-	}
-
+func set_weapon_state(new_state):
+	p_weapon_state = new_state
 	return
 
 func get_weapon_state():
-	return weapon_state
+	return p_weapon_state
 
 #-------------
 #-------------
 # Shield state
 
-func set_shield_state(state, dur):
-	shield_state = {
-	"state" : state,
-	"duration_timer" : dur
-	}
-
+func set_shield_state(new_state):
+	p_shield_state = new_state
 	return
 
 func get_shield_state():
-	return shield_state
-
-#-------------
-#-------------
-# Respawn state
-
-func set_respawn_state(tod, timer):
-	respawn_state = {
-	"time_of_death" 	: tod,
-	"respawn_timer" 	: timer
-	}
-
-	return
-
-func get_respawn_state():
-	return respawn_state
+	return p_shield_state
 
 #-------------
 
@@ -139,60 +129,78 @@ func get_respawn_state():
 
 
 # This method modifies the member vars
-func update_states(delta):  ## IMPURE
+func update_states(delta, state, condition_t):  ## PURE
 
-	# Check if there are any jumps queued and
-	# if we are at our destination.
-	var dests = get_jumps()["destinations"]
-	var pos = get_pos()
-	# Use while loop to catch any duplicates.
-	while dests.size() > 0 and pos == dests[0]:
-		dests.pop_front()
-		if dests.size() > 0:
-			set_jumps(pos, dests)
-		else:
-			set_jumps(null, [])
-			set_state(IDLE)
+#	var state = get_state()
+#	var ct = get_condition_timers()
 
-	# Check if stunned
-	if stunned_timer > 0:
-		stunned_timer -= delta
-		if not is_state(STUNNED):
-			return set_state(STUNNED)
-	elif is_state(STUNNED):
-		return set_state(IDLE)
+	## TODO: generalize the if conditions below ##
+#	for timer in condition_t:
+#		timer -=delta
+#		if timer <= 0:
+#			condition_t.erase(timer)
+#		elif state["condition"] != Condition
+	################################################
 
-	# Check if performing an action
-	if action_timer > 0:
-		action_timer -= delta
-		if not is_state(BUSY):
-			return set_state(BUSY)
-	elif is_state(BUSY):
-		return set_state(IDLE)
+	if not condition_t.empty():
+		# Check if stunned
+		if condition_t.has("stunned"):
+			condition_t["stunned"] -= delta
+			if condition_t["stunned"] <= 0:
+				condition_t.erase("stunned")
+			elif state["condition"] != Condition.STUNNED:
+				state["condition"] = Condition.STUNNED
+				return [state, condition_t]
+		# We will adjust state next time around instead of rechecking
+		elif state["condition"] == Condition.STUNNED:
+			state["action"] = Action.IDLE
+			return [state, condition_t]
 
-	# Check if supposed to respawn (and if dead)
-	var respawn = get_respawn_state()
-	if respawn["respawn_timer"] > 0:
-		respawn["respawn_timer"] -= delta
-		set_respawn_state(respawn)
-		if not is_state(DEAD):
-			return set_state(DEAD)
-	elif is_state(DEAD):
-		return set_state(RESPAWNING)
+		# Check if performing an action
+		if condition_t.has("busy"):
+			condition_t["busy"] -= delta
+			if condition_t["busy"] <= 0:
+				condition_t.erase("busy")
+			elif state["condition"] != Condition.BUSY:
+				state["condition"] = Condition.BUSY
+				return [state, condition_t]
+		# We will adjust state next time around instead of rechecking
+		elif state["condition"] == Condition.BUSY:
+			state["action"] = Action.IDLE
+			return [state, condition_t]
+
+		# Check if supposed to respawn (is dead)
+		if condition_t.has("respawn"):
+			condition_t["respawn"] -= delta
+			if condition_t["respawn"] <= 0:
+				condition_t.erase("respawn")
+			elif state["condition"] != DEAD:
+				state["condition"] = Condition.DEAD
+				return [state, condition_t]
+		# We will adjust state next time around instead of rechecking
+		elif state["condition"] == DEAD:
+			state["condition"] = Condition.RESPAWNING
+			return [state, condition_t]
+	if state["condition"] == Condition.DEAD:
+		condition_t["dead"] += delta
+	elif condition_t.has("dead"):
+		condition_t.erase("dead")
+
+	## TODO: Do something about this mess. It messes with my purity.
 
 	# Check the state of the shield as necessary
-	var shield = get_shield_state()
-	shield["state"] = Power.ON if shield["duration_timer"] > 0 else Power.OFF
-	if shield["state"] == Power.ON:
-		shield["duration_timer"] -= delta
-	set_shield_state(shield["state"], shield["duration_timer"])
-
-	# Check the state of the weapon and update if necessary
-	var weapon = get_weapon_state()
-	weapon["state"] = Power.OFF if weapon["cooldown_timer"] > 0 else Power.ON
-	if weapon["state"] == Power.OFF:
-		weapon["cooldown_timer"] -= delta
-	set_weapon_state(weapon["state"], weapon["target_loc"], weapon["cooldown_timer"])
+#	var shield = get_shield_state()
+#	shield["power"] = Power.ON if shield["timer"] > 0 else Power.OFF
+#	if shield["power"] == Power.ON:
+#		shield["timer"] -= delta
+#	set_shield_state(shield)
+#
+#	# Check the state of the weapon and update if necessary
+#	var weapon = get_weapon_state()
+#	weapon["power"] = Power.OFF if weapon["timer"] > 0 else Power.ON
+#	if weapon["power"] == Power.OFF:
+#		weapon["timer"] -= delta
+#	set_weapon_state(weapon)
 
 
 # Produce a random point inside a circle of a given radius
@@ -241,162 +249,174 @@ master func look_towards(point):  ## IMPURE
 
 # Get hit (and die - at least until better implementation is implemented)
 func hit():  ## IMPURE
-
-	if self.state != DEAD:
-		self.state = DEAD
-		set_monitorable(false)
-		set_hidden(true)
-
-		## Reset all active timers and states  ##
-		self.weapon["cooldown_timer"] = 0
-		self.stunned_timer = 0
-		self.action_timer = 0
-
-		self.weapon["target_loc"] = null
-		self.jump["active_jump_origin"] = null
-
-		update_states()
-		#########################################
-
-		# Set respawn timer based on elapsed game round time
-		self.time_of_death = GameRound.round_timer
-		self.respawn_timer = self.time_of_death / 10
-		print(self.respawn_timer)
-
-		var death_anim = preload("res://common/DeathEffect.tscn").instance()
-		death_anim.set_pos(get_pos())
-		get_parent().add_child(death_anim)
-
-		print(get_name() + " was killed and will be back in ", self.respawn_timer)
+	return
+#	var state = get_state()
+#	var condition_timers = get_condition_timers()
+#
+#	if state["condition"] != DEAD:
+#		state["condition"] = DEAD
+#
+#		## Reset all active timers and states  ##
+#		var wep_st = get_weapon_state()
+#		wep_state["timer"] = 0
+#		wep_state["target_loc"] = null
+#		condition_timers.clear()
+#		## TODO: Fix line below
+#		self.jump["active_jump_origin"] = null
+#
+#		set_monitorable(false)
+#		set_hidden(true)
+#		update_states()
+#		#########################################
+#
+#		# Set respawn timer based on elapsed game round time
+#		self.time_of_death = GameRound.round_timer
+#		self.timer = self.time_of_death / 10
+#		print(self.timer)
+#
+#		var death_anim = preload("res://common/DeathEffect.tscn").instance()
+#		death_anim.set_pos(get_pos())
+#		get_parent().add_child(death_anim)
+#
+#		print(get_name() + " was killed and will be back in ", self.timer)
 
 
 # Well, this one makes you respawn
 func respawn():  ## IMPURE
 
-	set_monitorable(true)  # Enable detection by other bodies and areas
+	set_monitorable(true)  # Enable detecondition_tion by other bodies and areas
 	set_pos(rand_loc(Vector2(0,0), 0, 1000))
 
 	set_shield_state(Power.ON, 2.0)
-	set_jumps(null, [])
+	rpc("set_path", { "from" : null, "to" : [] })
+#	set_path({ "from" : null, "to" : [] })
 
 
 # Attack given location (not relative to prog)
 master func attack(loc):  ## IMPURE
 
 #	var not_the_time_to_use_that = moving || busy
-	return set_state(BUSY if is_state(IDLE) && self.weapon["state"] == ON else get_state())
+#	var state = get_state()
+#	if state["action"] == Action.IDLE and get_weapon_state()["power"] == ON:
+#		state["action"] = Condition.BUSY
+#		set_state(state)
+	return
 #
-#		## PLACEHOLDER  ##
-#		GameRound.points += 1
-#		#################
+#		## PLACEHOLDER  ##########
+#		GameRound.points += 1  ##
+#		########################
 #
 #		# Spawn projectile
 #		var character_pos = get_pos()
 #		var projectile = preload("res://common/Projectile/Projectile.tscn").instance()
-#		var attack_dir = (self.weapon["target_loc"] - character_pos)
+#		var attack_dir = (gget_weapon_state()["target_loc"] - character_pos)
 #		attack_dir.y *= 2
 #		attack_dir = attack_dir.normalized()
 #
-#		projectile.destination = self.weapon["target_loc"]
+#		projectile.destination = gget_weapon_state()["target_loc"]
 #		projectile.set_global_pos( character_pos + attack_dir * Vector2(60,20) )
 #		get_parent().add_child(projectile)
 #
-#		self.weapon["cooldown_timer"] = weapon_cooldown
-#		self.action_timer = 0.2
+#		gget_weapon_state()["timer"] = weapon_cooldown
+#		self.state["timer"] = 0.2
 
 
 # This one stops your movement..
-sync func stop_moving():  ## IMPURE
-	set_monitorable(true)  # Can be detected by other bodies and areas
-	set_z(1)  # Back onto ground
-	get_node("Sprite").set_pos(Vector2(0, 0))  # y is jump height
-
-	set_jumps(null, get_jumps()["destinations"])
-	self.stunned_timer = JUMP_CD
-	return
+#sync func reset_motion_state():  ## IMPURE
+#	set_monitorable(true)  # Can be detected by other bodies and areas
+#	set_z(1)  # Back onto ground
+#	get_node("Sprite").set_pos(Vector2(0, 0))  # y is jump height
+#
+#	var p = get_path()
+#	p["from"] = null
+#	set_path(p)
+#
+#	apply_condition_timer("stunned", JUMP_CD)
+#
+#	return
 
 
 sync func animate_jump(jump_height):  ## IMPURE
-	var sprite_pos = Vector2(0, -1) * jump_height
-	var shadow_scale = ( 1 - 0.08 * sin(deg2rad(-1 * jump_height)) )
-	var shadow_opacity = shadow_scale
-	shadow_scale *= Vector2(1, 1)
+	# If we're not in the air, just set everything
+	# accordingly and skip the calculations.
+	if jump_height <= 0:
+		set_monitorable(true)  # Can be detected by other bodies and areas
+		set_z(1)  # Back onto ground
+		get_node("Sprite").set_pos(Vector2(0, 0))  # y is jump height
 
-	get_node("Sprite").set_pos(sprite_pos)
-	get_node("Shadow").set_scale(shadow_scale)
-	get_node("Shadow").set_opacity(shadow_opacity)
-	set_z(jump_height + 1)  # To render after everything below
+		var path = get_path()
+		path["from"] = null
+		path["to"].pop_front()
+		set_path(path)
+
+		apply_condition_timer("stunned", JUMP_CD)
+	else:
+		var sprite_pos = Vector2(0, -1) * jump_height
+		var shadow_scale = ( 1 - 0.08 * sin(deg2rad(-1 * jump_height)) )
+		# Use shadow scale as a basis for the opacity too
+		var shadow_opacity = shadow_scale
+		# Then convert the scale into the proper type
+		shadow_scale *= Vector2(1, 1)
+
+		get_node("Sprite").set_pos(sprite_pos)
+		get_node("Shadow").set_scale(shadow_scale)
+		get_node("Shadow").set_opacity(shadow_opacity)
+		set_z(jump_height + 1)  # To render after everything below
 	return
 
 
-sync func set_motion_state(motion_state):  ## IMPURE
+sync func set_motion_state(path, state, condition_timers):  ## IMPURE
 
-	var pos = get_pos()
-	var dests = get_jumps()["destinations"]
+	# Check if there are any jumps queued and
+	# if so pop any that hold our current pos.
+	# Use while loop to catch any duplicates.
+	while path["to"].size() > 0 and state["position"] == path["to"][0]:
+		path["to"].pop_front()
+		path["from"] = state["position"] if path["to"].size() > 0 else null
+		set_path(path)
 
-	animate_jump(motion_state["jump_height"])
-#	rpc("animate_jump", motion_state["jump_height"])
+	animate_jump(state["height"])
 
-	# Check if moving
-	if (motion_state["motion"].length() > 0 or
-			dests.size() > 0):
-		set_monitorable(false)  # Disable detection by other bodies and areas
-		set_pos(pos + motion_state["motion"])
-		if not is_state(MOVING):
-			set_state(MOVING)
-	elif is_state(MOVING):
-		return set_state(STUNNED)
+	# Check if (supposed to be) moving and apply motion
+	if (state["motion"].length() > 0 or path["to"].size() > 0):
+		# Disable detection by other bodies and areas.
+		# This is to avoid hitting or being hit by anything while jumping.
+		set_monitorable(false)
+
+		set_pos(state["position"] + state["motion"])
+	# Stun on landing
+	elif state["action"] == Action.MOVING:
+		condition_timers["stunned"] += JUMP_CD
 
 
 # Update the state of motion to reflect what is desired
-master func new_motion_state(delta, init_pos, pos, dest):  ## PURE
+master func new_motion_state(delta, path, state):  ## PURE
 
-	var motion
-	var jump_height
-
-	var dist_covered = pos - init_pos
+	var dist_covered = state["position"] - path["from"]
 	dist_covered.y *= 2
 	dist_covered = dist_covered.length()
 
-	var dist_total = dest - init_pos
+	var dist_total = path["to"][0] - path["from"]
 	dist_total.y *= 2
 	dist_total = dist_total.length()
 
-	var dir = dest - pos
+	var dir = path["to"][0] - state["position"]
 	dir.y *= 2
 	dir = dir.normalized()
 
 
 	# Where to put ourselves next
 	var speed = min(dist_total*2, MAX_SPEED)
-	motion = dir * speed * delta
-	motion.y *= 0.5
-	var coming_in_hot = motion.length() > 0 && motion.length() >= pos.distance_to(dest)
-	motion = motion if not coming_in_hot else ( dest - pos )
+	state["motion"] = dir * speed * delta
+	state["motion"].y *= 0.5
+	var coming_in_hot = state["motion"].length() > 0 && state["motion"].length() >= state["position"].distance_to(path["to"][0])
 	if coming_in_hot:
-		pass
+		state["motion"] = path["to"][0] - state["position"]
 
 	var jump_completion = dist_covered / dist_total if dist_total > 0 else 1
-	jump_height = sin(deg2rad(180*jump_completion)) * dist_total * 0.2
+	state["height"] = sin(deg2rad(180*jump_completion)) * dist_total * 0.2
 
-	# Bundle up and return the new state in a nice little dict
-	return {
-		"motion"		: 	motion,
-		"jump_height" 	: 	jump_height,
-	}
-
-
-# This chec... ugh just read the name
-#master func should_be_moving():
-#	var limit = 2  # Jump queue limit
-#	var dests = self.jump["destinations"]
-#	# Check if any jumps are queued
-#	if dests.size() < 1:
-#		return false
-#	# Limit amount of queued jumps allowed, add one because active dest is not cleared until landing
-#	if dests.size() > limit:
-#		self.jump["destinations"].resize(limit + 1)
-#	return true if dests[0] != get_pos() else false
+	return state
 
 
 #####################################################################
--- game/scripts/Player.gd
+++ game/scripts/Player.gd
@@ -3,10 +3,10 @@ extends "res://scripts/Character.gd"
 var mouse_pos = Vector2()
 
 # Spawn an NPC to play with
-func spawn_enemy(loc):
-	var enemy = preload("res://npc/Bot.tscn").instance()
-	enemy.set_pos(loc)
-	get_parent().add_child(enemy)
+#sync func spawn_enemy(loc):
+#	var enemy = preload("res://npc/Bot.tscn").instance()
+#	enemy.set_pos(loc)
+#	get_parent().add_child(enemy)
 
 
 # Display an indicator for where you clicked
@@ -25,33 +25,33 @@ func spawn_click_indicator(pos, anim):
 func _fixed_process(delta):
 
 	# Update all states, timers and other statuses and end processing here if stunned
-	update_states(delta)
-	if is_state(STUNNED):
+	update_states(delta, get_state(), get_condition_timers())
+
+	if get_state()["condition"] == STUNNED:
 		return
 
 	if self.is_network_master():
 
-		var dests = get_jumps()["destinations"]
-		var init_pos = get_jumps()["active_jump_origin"]
+		var path = get_path()
 		var weapon = get_weapon_state()
 		var pos = get_pos()
 		rset_unreliable("slave_pos", pos)
 
-		if dests.size() > 0:
-			if init_pos == null:
-				init_pos = pos
-				set_jumps(init_pos, dests)
-			rpc("set_motion_state", new_motion_state(delta, init_pos, pos, dests[0]))
-			if dests.size() > JUMP_Q_LIM:
-				dests.resize(JUMP_Q_LIM + 1)
+		if path["to"].size() > 0:
+			if path["to"].size() > JUMP_Q_LIM:
+				path["to"].resize(JUMP_Q_LIM + 1)
+			if path["from"] == null:
+				path["from"] = pos
+			set_path(path)
+			rpc("set_motion_state", path, new_motion_state(delta, path, get_state()))
 #		else:
 #			rpc("set_motion_state", { "motion" : Vector2(0,0), "jump_height" : 0 })
 
 		if weapon["target_loc"] != null:
 			attack(weapon["target_loc"])
 
-		var focus = weapon["target_loc"] if is_state(BUSY) else ( dests[0] if is_state(MOVING) else mouse_pos )
-		rset_unreliable("slave_focus", focus)
+		var focus = weapon["target_loc"] if get_state() == BUSY else ( path["to"][0] if get_state() == MOVING else mouse_pos )
+		rset("slave_focus", focus)
 		look_towards(focus)
 	else:
 		look_towards(slave_focus)
@@ -65,12 +65,11 @@ func _fixed_process(delta):
 func _unhandled_input(ev):
 	mouse_pos = get_global_mouse_pos()
 	if Input.is_action_just_pressed("move_to"):
-		var dests = get_jumps()["destinations"]
-		var init_pos = get_jumps()["active_jump_origin"]
-		if not (( dests.size() > 0 ) and ( mouse_pos == dests.back() )):
-			init_pos = init_pos if dests.size() > 0 else get_pos()
-			dests.append(mouse_pos)
-			set_jumps(init_pos, dests)
+		var path = get_path()
+		if not (( path["to"].size() > 0 ) and ( mouse_pos == path["to"].back() )):
+			path["from"] = path["from"] if path["to"].size() > 0 else get_pos()
+			path["to"].append(mouse_pos)
+			set_path(path)
 			spawn_click_indicator(mouse_pos, "move_to")
 	if Input.is_action_just_pressed("attack"):
 		var weapon = get_weapon_state()
